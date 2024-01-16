855413d42d20cd3927c359982566cb03596c77cf
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-01-16 09:47:28 +0100

    snack 1

--- Palidroma/main.js
+++ Palidroma/main.js
@@ -1,2 +1,18 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+let parola = prompt( "dammi una parola" )
+
+function palindroma( word ){
+
+let parolaAlContrario = ``
+
+for( let i = word.lenght - 1; i >= 0; i-- ){
+    parolaAlContrario += word[i]
+}
+
+return parolaAlContrario
+
+}
+
+console.log( palindroma( parola ) )
